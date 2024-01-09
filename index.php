3bab6b313f421a206e6153e8a34734a3aa3795b0
Vincenzo Tito <vincenzotito@icloud.com>
2024-01-09 19:26:01 +0100

    Traccia 2

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
-
+// traccia 1
 $integer = 3;
 $float = 1.50;
 $stringa = "Ciao sono la mia prima stringa in php";
 $booleano = true;
 
-var_dump($integer, $float, $stringa, $booleano);
+// var_dump($integer, $float, $stringa, $booleano);
+
+// traccia 2
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè?";
+$text6 = $text2;
+$text7 = "bevuto";
+$text8 = "tutto";
+
+echo $text1 .' '. $text2 . ' '. $text3 . ' ' . $text4 . ' ' . $text5 . ' ' . $text6 . ' ' . $text7;
+
